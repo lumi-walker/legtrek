63435a369064656c086780eea081f3950f998472
min-dai <34080489+min-dai@users.noreply.github.com>
2019-05-17 20:29:07 -0700

    Update motor_calib_cons.h

--- src/libraries/Motor/motor_calib_cons.h
+++ src/libraries/Motor/motor_calib_cons.h
@@ -1,3 +1,6 @@
+#ifndef MOTOR_CALIB_CONS_H
+#define MOTOR_CALIB_CONS_H
+
 float DEFAULT_ACCEL = 500.0f;
 bool FORWARD = true;
 bool REVERSE = false;
@@ -5,3 +8,5 @@ int max_acc_rpmps = 956;
 int min_acc_rpmps = 483;
 float minSpeed = 0.24; //minimum limit in set speed
 float maxSpeed = 1.2; //maximum limit in set speed
+
+#endif
